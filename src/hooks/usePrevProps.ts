a74e5613e3cae2073dfa88c5785730ec57e688f3
dattranbenit <dattran.benitl@gmail.com>
2022-03-31 17:46:51 +0700

    commit

--- src/hooks/usePrevProps.ts
+++ src/hooks/usePrevProps.ts
@@ -9,10 +9,8 @@ import { useEffect, useRef } from "react";
  */
 export const usePrevProps = <K = any>(value: K) => {
   const ref = useRef<K>();
-
   useEffect(() => {
     ref.current = value;
   });
-
   return ref.current;
 };
